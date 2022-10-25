9e396157d3dd246b545b2725ee620b46e415784d
chiyoung <100222045+Returnmakdo@users.noreply.github.com>
2022-10-25 23:43:30 +0900

    kakaologin중

--- src/pages/KaKaoLogin.jsx
+++ src/pages/KaKaoLogin.jsx
@@ -7,27 +7,48 @@ import styled from "styled-components";
 import { useCookies } from "react-cookie";
 
 function KaKaoLogin() {
+  //   useEffect(() => {
+  //     let params = new URL(document.location.toString()).searchParams;
+  //     let code = params.get("code"); // 인가코드 받는 부분
+  //     let grant_type = "authorization_code";
+  //     let client_id = REST_API_KEY;
+
+  //     axios
+  //       .post(
+  //         `https://kauth.kakao.com/oauth/token?
+  //             grant_type=${grant_type}
+  //             &client_id=${client_id}
+  //             &redirect_uri=${REDIRECT_URI}
+  //             &code=${code}`,
+  //         {
+  //           headers: {
+  //             "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+  //           },
+  //         }
+  //       )
+  //       .then((res) => {
+  //         console.log("res", res);
+  //         // res에 포함된 토큰 받아서 원하는 로직을 하면된다.
+  //       });
+  //   }, []);
   const location = useLocation();
   const navigate = useNavigate();
   const KAKAO_CODE = location.search.split("=")[1]; // 인가코드 전달하기 백으로
   const [cookies, setCookie, removeCookie] = useCookies(["accessToken"]);
   const getKaKaoToken = () => {
     // 백에서
-    fetch("https://kauth.kakao.com/oauth/token", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: `grant_type=authorization_code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${KAKAO_CODE}`,
-    })
+    // fetch("http://localhost:4000/auth/kakao", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    //   body: `grant_type=authorization_code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${KAKAO_CODE}`,
+    // })
+    axios
+      .post("http://localhost:4000/auth/kakao")
       .then((res) => res.json())
       .then((data) => {
         // 백엔드에 토큰전달
-
         console.log(data);
-        if (data.access_token) {
-          setCookie("refreshToken", data.refresh_token);
-          setCookie("accessToken", data.access_token);
-          navigate("/");
-        }
+        navigate("/");
       });
     //   .then((res) => res.json())
     //   .then((data) => {
